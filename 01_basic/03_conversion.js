57eaa15f106db2b29f6334746e0b68ad81a7baaa
Vikash Kumar Ray <vikash00438@gmail.com>
2026-05-20 19:51:53 +0530

    Add file

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,48 @@
+
+//*********************************************************** :-  conversion  -:   **************************************************************** 
+
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let  valueInNumbar = Number(score);
+// console.log(typeof (valueInNumbar));
+
+
+let temprature = 0
+console.log(typeof number);
+
+
+let valueInString = String(temprature);
+
+console.log(temprature);
+
+console.log(typeof (valueInString));
+
+
+
+
+//*********************************************************** :-  conversion  -:   **************************************************************** 
+
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let  valueInNumbar = Number(score);
+// console.log(typeof (valueInNumbar));
+
+
+let temprature = 0
+console.log(typeof number);
+
+
+let valueInString = String(temprature);
+
+console.log(temprature);
+
+console.log(typeof (valueInString));
+
+
+
---
+++ 01_basic/04_Operations.js
@@ -0,0 +1,50 @@
+// ****************************************************:-   Operations   -:*************************************************************************
+
+
+
+// let value = 3
+
+// let negvalue = -value
+// console.log (negvalue);
+
+
+// console.log (3 + 3);
+// console.log (3 - 3);
+// console.log (3 * 3);
+// console.log (2 ** 2);
+// console.log (2 ** 3);
+// console.log (3 / 3);
+// console.log (3 % 3);
+
+
+let str1 = "hello"
+let str2 = " vikash"
+let str3 = str1 + str2
+
+console.log(str3);
+
+// ****************************************************:-   Operations   -:*************************************************************************
+
+
+
+// let value = 3
+
+// let negvalue = -value
+// console.log (negvalue);
+
+
+// console.log (3 + 3);
+// console.log (3 - 3);
+// console.log (3 * 3);
+// console.log (2 ** 2);
+// console.log (2 ** 3);
+// console.log (3 / 3);
+// console.log (3 % 3);
+
+
+let str1 = "hello"
+let str2 = " vikash"
+let str3 = str1 + str2
+
+console.log(str3);
+
