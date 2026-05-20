ff2b5042b9f504a6600046df7fabcd4ed540d6c6
Vikash Kumar Ray <vikash00438@gmail.com>
2026-05-20 19:50:04 +0530

    This is my first commit

---
+++ 01_basic/01_basic.js
@@ -0,0 +1,35 @@
+//accountId = 23132
+//console.log(accountId)                // she is right  But is not good for programmer 
+
+
+//let accountId = 11111111111     // let is changeble and we are control 
+//console.log(accountId)         //It is good for programmer
+
+
+//const accountId = 1234        //const does not  changable    
+//console.log(accountId)       
+
+
+//var name = vikash            // var is not use becouse ise ek jagah change karoge but ye by defoult other jagah bhi chanlge ho jata hai 
+//console.log(name)             // var is not control
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------//
+
+/* let accountId = "238746"             
+let accountName = "vikash"
+let accountEmail = "vikash00438@gmail.com"       // " " It is right variable me value dene ke liye 
+let accountPassword = 12345                      // But integer or number ke liye jaruri nhi hai
+let accountCity = "varanasi"
+
+console.log( "Id :",accountId);
+console.log("Name :",accountName);
+console.log( "Gmail :",accountEmail);
+console.log( "Password :",accountPassword);
+console.log( "City :",accountCity); */
+
+
+
+let accountName = "vikash"
+console.log(accountName);
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,165 @@
+ //************************************************************ :-     Datatypes   -:   ******************************************************************* 
+
+            // number => 2 to power 53
+            //  bigint
+            //  string => ""
+            //  boolean => true / false
+            //  null => standalone value
+            //  undefined =>
+             //  symbol => unique
+            
+            // object
+
+
+
+"use strict";       // treat all JS code as newer version .
+
+ //alert ( 3 + 4)        //we are using node js , not browser.
+
+// console.log(3 + 4);    // code readable hona chahiye.
+
+
+let name = "vikash"
+let age = 19
+let isLoggedIn = false
+let account = null
+let city
+
+console.log (typeof "vikash");
+
+console.log (typeof age);
+
+console.log(typeof isLoggedIn);
+
+console.log(typeof account);
+
+console.log(typeof city)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
