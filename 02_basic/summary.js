7b8f864e9d17b6405fca5606dcab446887679839
Vikash Kumar Ray <vikash00438@gmail.com>
2026-05-24 11:40:21 +0530

    Add a new file

---
+++ 02_basic/summary.js
@@ -0,0 +1,42 @@
+// const users_detail = {
+
+// username: "vikash",
+// password:  123456,
+// gmail:  "vikash@gmail.com",
+// pone_No:  7854524871,
+
+// }
+
+// console.log(users_detail.password);
+
+
+
+// let myDate = new Date()
+
+// console.log(myDate); 
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+
+// const city_name = ["Delhi", "Varanasi", "Goa", "Allahabad"];
+// console.log(city_name);
+
+
+const girl_name = ["Kajal", "Radha", "Sita", "Ragini"];
+const boy_name = ["Ram", "Shyam", "Vikash","Aman"];
+
+girl_name. push(boy_name);
+console.log(girl_name);
+
+console.log(girl_name [2] [3]);
+
+
+let marks1 = 66;
+let marks2 = 70;
+let marks3 = 80;
+let marks4 = 90;
+let marks5 = 80;
+
+console.log(Array.of(marks1, marks2, marks3, marks4, marks5));
+
