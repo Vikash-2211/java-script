885ab6f3dd8bec24c85fd7f4254b028ac8e36bb3
Vikash Kumar Ray <vikash00438@gmail.com>
2026-06-02 15:56:25 +0530

    add changes

--- 04_control_flow/01_loop.js
+++ 04_control_flow/01_loop.js
@@ -63,3 +63,21 @@ if(score < 700){
         console.log("score is less than 1100");
     }
 
+
+const UserLoggedIn = true
+const DebitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(UserLoggedIn && DebitCard){
+
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromEmail || loggedInFromGoogle){
+    
+    console.log("User logged in");
+}
+
+
+
