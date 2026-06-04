8fa4b6a0298448c7eb94e693669cfeaf763cb92e
Vikash Kumar Ray <vikash00438@gmail.com>
2026-06-04 19:47:20 +0530

    add new file

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,81 @@
+// --------------------- switch ------------------
+
+
+/*
+const month = 3
+
+switch (month) {
+
+    case 1:
+        console.log("january");
+        break;
+
+    case 2:
+        console.log("february");
+        break;
+
+    case 3:
+        console.log("march");
+        break;
+
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("not match your list");
+
+}
+
+*/
+
+/*
+const name = "vikash"
+
+switch (name) {
+
+    case "samir":
+        console.log("your name : samir");
+        break;
+
+    case "vinay":
+        console.log("your name : vinay");
+        break;
+
+    case "vikash":
+        console.log("your name : vikash");
+        break;
+
+    default:
+        console.log("your name not list :");
+
+
+}
+
+*/
+
+
+const result = 73
+
+switch (result) {
+
+    case 39:
+        console.log("your result is 39");
+        break;
+
+    case 54:
+        console.log("your result is 54");
+        break;
+
+    case 66:
+        console.log("your result is 66");
+        break;
+
+    case 88:
+        console.log("your result is 88");
+        break;
+
+    default:
+        console.log("your result is not match this list")
+
+}
